7103c17de16ce11200b91fa8e4d86585dc601bff
Victory Asokomeh <victoryasokomeh@gmail.com>
2019-10-14 01:15:55 +0100

    added auth route and refactored mongoose config

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const usersRoute = require('./routes/users');
+const authRoute = require('./routes/auth');
 
 require('dotenv').config();
 const app = express()
@@ -12,12 +13,20 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(morgan('dev'));
 
+app.use('/api/', authRoute);
 app.use('/api/users', usersRoute);
 
-mongoose.set("useUnifiedTopology", true)
-mongoose.set("useNewUrlParser", true)
-mongoose.connect(process.env.DB_CONNECTION, () => console.log('connected to DB successfully'))
+const dbOptions = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+}
 
-const PORT = process.env.port || 4000
+const uri = process.env.DB_CONNECTION
+mongoose.connect(uri, dbOptions)
+    .then(() =>console.log('connected to DB'))
+    .catch((error) => console.log('Failed to connect to db: ', error));
+
+const PORT = process.env.port || 4000 
 app.listen(PORT, () => console.log(`Now listening on port ${PORT}`))
 
+       
